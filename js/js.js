fbb6556c8ebc53d1cce53d1596dc67d776a6a84d
Egor Vinogradov <egor@ostrovok.ru>
2012-04-12 17:08:32 +0400

    Find method fix

--- js/js.js
+++ js/js.js
@@ -1,10 +1,15 @@
 (function(d){
 
     var $,
+        Settings,
         Utils,
         Query,
         Methods;
 
+
+    Settings = {
+        ua: navigator.userAgent
+    };
 
 
     Utils = {
@@ -56,7 +61,7 @@
                     : false;
             };
 
-            if ( nodes && nodes.length && nodes instanceof Object ) {
+            if ( nodes && (( nodes.length && nodes instanceof Object ) || nodes instanceof Node ) ) {
 
                 result =
                     nodes instanceof Array && checkNodes(nodes) ||
@@ -145,33 +150,37 @@
             return prop;
         },
 
-
         // Contains information about browsers and vendor prefixes
 
         browser: {
             msie: {
-                test: /msie/i.test(this.ua),
-                prefix: 'Ms'
+                test: /msie/i.test(Settings.ua),
+                prefix: 'ms'
             },
             webkit: {
-                test: /webkit/i.test(this.ua),
-                prefix: 'Webkit'
+                test: /webkit/i.test(Settings.ua),
+                prefix: 'webkit'
             },
             gecko: {
-                test: /gecko/i.test(this.ua),
-                prefix: 'Moz'
+                test: /gecko/i.test(Settings.ua),
+                prefix: 'moz'
             },
             opera: {
-                test: /opera/i.test(this.ua),
-                prefix: 'O'
+                test: /opera/i.test(Settings.ua),
+                prefix: 'o'
             }
-        },
-        ua: navigator.userAgent
+        }
     };
 
 
 
     // Returns node collection for specified selector
+    // Examples:
+    //    Query('body')
+    //    Query('.class')
+    //    Query('#id')
+    //    Query('.class, body, #id')
+    //    Query('.class', container)
 
     Query = function(s, parentNode){
 
@@ -179,60 +188,86 @@
             getFromNodes,
             isSelector = typeof s === 'string' || s instanceof String;
 
-        getBySelector = function(selector){
 
-            var getByTagName,
-                getByClassName,
-                getById,
-                tagNameRe = /^\w+$/i,
-                classNameRe = /^\.[a-z0-9-_.]+$/i,
-                idRe = /^#[a-z0-9-_]+$/i,
-                result;
 
-            parentNode = parentNode || d;
 
-            getByTagName = function(selector){
-                var result = [];
-                Utils.each(parentNode.getElementsByTagName(selector), function(){
-                    result.push(this);
-                });
+        getBySelector = function(s){
+
+            var selectors = s.split(','),
+                results = [],
+                getNodes;
+
+
+            getNodes = function(selector){
+
+                var getByTagName,
+                    getByClassName,
+                    getById,
+                    tagNameRe = /^\w+$/i,
+                    classNameRe = /^\.[a-z0-9-_.]+$/i,
+                    idRe = /^#[a-z0-9-_]+$/i,
+                    result;
+
+                parentNode = parentNode && parentNode instanceof Node
+                    ? parentNode
+                    : parentNode && parentNode.length && parentNode[0] instanceof Node
+                        ? parentNode[0]
+                        : d;
+
+                console.log('parent node', parentNode);
+
+                getByTagName = function(selector){
+                    var result = [];
+                    Utils.each(parentNode.getElementsByTagName(selector), function(){
+                        result.push(this);
+                    });
+                    return result;
+                };
+
+                getByClassName = function(selector){
+                    var result = [],
+                        nodes = parentNode.getElementsByClassName
+                            && parentNode.getElementsByClassName(selector.substr(1));
+
+                    Utils.each(nodes, function(){
+                        result.push(this);
+                    });
+                    return result;
+                };
+
+                getById = function(selector){
+                    var result = parentNode.getElementById(selector.substr(1));
+                    return result
+                        ? [result]
+                        : [];
+                };
+
+
+                // TODO: remove console log
+                console.log('Node type:',
+                    tagNameRe.test(selector) && 'TAG  ' + selector ||
+                    classNameRe.test(selector) && 'CLASS  ' + selector ||
+                    idRe.test(selector) && 'ID  ' + selector
+                );
+
+
+                result =
+                    tagNameRe.test(selector)    && getByTagName(selector)   ||
+                    classNameRe.test(selector)  && getByClassName(selector) ||
+                    idRe.test(selector)         && getById(selector)        ||
+                    [];
+
                 return result;
-            };
 
-            getByClassName = function(selector){
-                var result = [],
-                    nodes = parentNode.getElementsByClassName
-                        && parentNode.getElementsByClassName(selector.substr(1));
-
-                Utils.each(nodes, function(){
-                    result.push(this);
-                });
-                return result;
-            };
-
-            getById = function(selector){
-                var result = parentNode.getElementById(selector.substr(1));
-                return result
-                    ? [result]
-                    : [];
             };
 
 
-            // TODO: remove console log
-            console.log('Node type:',
-                tagNameRe.test(selector) && 'TAG  ' + selector ||
-                classNameRe.test(selector) && 'CLASS  ' + selector ||
-                idRe.test(selector) && 'ID  ' + selector
-            );
+            for ( var i = 0, l = selectors.length; i < l; i++ ) {
+                results = results.concat(getNodes(Utils.trim(selectors[i])));
+            }
 
+            return results;
 
-            result =
-                tagNameRe.test(selector)    && getByTagName(selector)   ||
-                classNameRe.test(selector)  && getByClassName(selector) ||
-                idRe.test(selector)         && getById(selector)        ||
-                [];
-
-            return result;
         };
 
 
@@ -243,6 +278,7 @@
                 ? result
                 : [];
         };
+
 
         return isSelector
             ? getBySelector(s)
@@ -278,14 +314,14 @@
         },
 
 
-        // Finds children of node (nodes)
+        // Finds children of nodes
 
         find: function(selector){
-
+            
             var result = [];
 
             this.each(function(){
-                result.push( Query(selector, this) );
+                result = result.concat( Query(selector, this) );
             }, this);
 
             return result;
@@ -323,8 +359,8 @@
             var replace,
                 setAlias,
                 alias = {
-                    size: 'width|height|top|left|right|bottom|marginTop|marginBottom|marginLeft|marginRight|paddingTop|paddingBottom|paddingLeft|paddingRight',
-                    css3prop: 'mozBoxShadow|MozBoxShadow|webkitBoxShadow|WebkitBoxShadow|mozborderRadius|MozborderRadius|webkitBorderRadius|WebkitBorderRadius'
+                    size: 'width|height|top|left|right|bottom|marginTop|marginBottom|marginLeft|marginRight|paddingTop|paddingBottom|paddingLeft|paddingRight'.split('|'),
+                    css3prop: 'mozBoxShadow|MozBoxShadow|webkitBoxShadow|WebkitBoxShadow|mozborderRadius|MozborderRadius|webkitBorderRadius|WebkitBorderRadius'.split('|')
                 };
 
             replace = {
@@ -352,8 +388,8 @@
             };
 
 
-            setAlias(alias.size.split('|'), replace.size);
-            setAlias(alias.css3prop.split('|'), replace.css3prop);
+            setAlias(alias.size, replace.size);
+            setAlias(alias.css3prop, replace.css3prop);
 
 
             this.each(this, function(){
@@ -392,7 +428,6 @@
 
 
 
-
     (function(p){
         for ( var method in Methods ) {
             p[method] = Methods[method];
@@ -401,197 +436,13 @@
 
 
 
-
-
-
-
     if ( !window.console || !window.console.log ) {
         window.console = {
             log: function(){
-                alert(arguments.join(' '));
+                alert(Array.prototype.join.call(arguments, ' '));
             }
         };
     }
-
-
-
-
-    
-
-    var $2 = function(selector){
-
-        var elements = new Query(selector);
-
-
-        // TODO: RTFM and do it better
-
-
-        // Returns collection containing first element of node collection
-        elements.first = function(){
-            return [this[0]];
-        };
-
-
-        // Returns collection containing last element of node collection
-        elements.last = function(){
-            return [this[ this.length - 1 ]];
-        };
-
-
-        // Returns collection containing element with specified index of node collection
-        elements.eq = function(i){
-            return [this[i]];
-        };
-
-
-
-
-
-
-        // Returns array containing inner HTML of collection elements or
-        // sets HTML to elements of node collection if argument specified
-        elements.html = function(str){
-
-            var result = [],
-                getHTML = function(){
-                    result.push(this.innerHTML);
-                },
-                setHTML = function(){
-                    this.innerHTML = str;
-                };
-
-            this.each(this, str ? setHTML : getHTML);
-
-            return str
-                ? this
-                : result.length === 1
-                    ? result[0]
-                    : result;
-        };
-
-
-        elements.css = function(options){
-
-
-            //(/.*?((?:(?:-)?[0-9]+(?:px|%)?|left|right|top|bottom|center|inherit)).*?/ig, '$1 ')
-
-            var replace = {
-                
-                    background: function(prop, val){
-
-                        var re = {
-                                image: /.*((?:url\(.*\)|(?:(?:-.+)?-)?.+-gradient\(.*\))).*/i,
-                                position: /.*?((?:(?:-)?[0-9]+(?:px|%)?|left|right|top|bottom|center))\s+?((?:(?:-)?[0-9]+(?:px|%)?|left|right|top|bottom|center)).*/i,
-                                repeat: /.*(no-repeat|repeat-x|repeat-y|[^-]repeat|round|space).*/i,
-                                important: /!important/i
-                            },
-                            color,
-                            image,
-                            position,
-                            repeat,
-                            none = {
-                                backgroundColor: 'transparent',
-                                backgroundImage: 'none'
-                            };
-
-                        if ( Utils.trim(val) === 'none' ) return none;
-
-
-
-//function zzz(str, p1, p2, offset, s) {
-//
-//    console.log('str:', str, '\np1:', p1, '\np2:', p2, '\noffset:',offset, '\ns:',s);
-//    //return p1 + ", " + p2;
-//}
-//
-//var newString = "my XXzz".replace(/(X+)(z+)/, zzz);
-
-
-
-
-
-
-                        image = val.replace(re.image, '$1');
-                        repeat = val.replace(re.repeat, '$1');
-                        position = val.replace(re.position, '$1 $2');
-
-                        // TODO: parse background color
-                        // TODO: fix background image regexp
-                        // TODO: improve background position regexp
-
-                        return {
-                            backgroundColor: color,
-                            backgroundImage: image,
-                            backgroundPosition: position,
-                            backgroundRepeat: repeat
-                        };
-                    },
-                    css3prop: function(prop, val){
-
-                        var result = {};
-                        result[Utils.setVendorPrefix(prop)] = val;
-                        return result;
-                    },
-                    size: function(prop, val){
-
-                        var result = {};
-                        result[prop] = +val
-                            ? val + 'px'
-                            : val;
-
-                        return result;
-                    }
-                };
-
-            replace.width =
-            replace.height =
-            replace.top =
-            replace.left =
-            replace.right =
-            replace.bottom =
-            replace.marginTop =
-            replace.marginBottom =
-            replace.marginLeft =
-            replace.marginRight =
-            replace.paddingTop =
-            replace.paddingBottom =
-            replace.paddingLeft =
-            replace.paddingRight = replace.size;
-
-            replace.mozBoxShadow        = replace.MozBoxShadow =
-            replace.webkitBoxShadow     = replace.WebkitBoxShadow =
-            replace.mozborderRadius     = replace.MozborderRadius =
-            replace.webkitBorderRadius  = replace.WebkitBorderRadius = replace.css3prop;
-
-            this.each(this, function(){
-
-                for ( var a in options ) {
-
-                    var property = a,
-                        value = options[a],
-                        substitution;
-
-                    if ( property in replace ) {
-
-                        substitution = replace[property](property, value);
-
-                        for ( var newProp in substitution ) {
-                            this.style[newProp] = substitution[newProp];
-                        }
-                        continue;
-                    }
-
-                    this.style[property] = value;
-                }
-            });
-
-            return this;
-        };
-
-
-
-        return elements;
-    };
 
 
 
